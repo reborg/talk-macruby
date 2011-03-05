8c0eb5d2a83798eef0bfbc4083045a2ad4db358d
reborg <reborg@reborg.net>
2011-03-05 12:26:59 +0100

    Added the text expander snippets plus renaming the solved project

--- src/Shortify/config/environment.rb
+++
@@ -1,7 +0,0 @@
-APP_ROOT = File.expand_path(File.dirname(__FILE__) + '/..')
-['bacon/lib', 'mocha/lib'].each do |path|
-  $LOAD_PATH << APP_ROOT + '/vendor/gems/' + path
-end
-['/Classes'].each do |path|
-  $LOAD_PATH << APP_ROOT + path
-end
